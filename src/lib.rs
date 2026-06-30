4b2d50d40e2b4c106621fbcb47dcacb1f6fd3360
Luis Schwab <luisschwab@protonmail.com>
2026-06-30 18:40:58 -0300

    {wip} chore(deps): bump `floresta` to *

--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,18 @@
 
 #![doc = include_str!("../README.md")]
 
-pub use floresta_chain::BlockConsumer;
-pub use floresta_chain::UtxoData;
-pub use floresta_chain::pruned_utreexo::chainparams::AssumeUtreexoValue;
-pub use floresta_chain::pruned_utreexo::chainparams::ChainParams;
-pub use floresta_wire::TransportProtocol;
-pub use floresta_wire::UtreexoNodeConfig;
-pub use floresta_wire::node::ConnectionKind;
-pub use floresta_wire::node::PeerStatus;
-pub use floresta_wire::node::UtreexoNode;
-pub use floresta_wire::node::running_ctx::RunningNode;
-pub use floresta_wire::node_interface::PeerInfo;
-pub use floresta_wire::rustreexo;
+pub use floresta::chain::BlockConsumer;
+pub use floresta::chain::UtxoData;
+pub use floresta::chain::pruned_utreexo::chainparams::AssumeUtreexoValue;
+pub use floresta::chain::pruned_utreexo::chainparams::ChainParams;
+pub use floresta::wire::TransportProtocol;
+pub use floresta::wire::UtreexoNodeConfig;
+pub use floresta::wire::node::ConnectionKind;
+pub use floresta::wire::node::PeerStatus;
+pub use floresta::wire::node::UtreexoNode;
+pub use floresta::wire::node::running_ctx::RunningNode;
+pub use floresta::wire::node_interface::PeerInfo;
+pub use floresta::wire::rustreexo;
 
 pub mod client;
 #[cfg(feature = "logger")]
